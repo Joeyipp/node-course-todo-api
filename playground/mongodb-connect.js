e2b42ef5fab4d160e36387a33ca5cf846af8a253
Joey Yip <Joey@knoesis.org>
2017-10-23 16:04:10 -0400

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,62 @@
+// ObjectID is a 12 byte value (1st 4 bytes is timestamp, next 3 bytes are machine-identifier, next 2 bytes are process id, next 3 byte-counter)
+// ObjectID is the default way MongoDB creates id, but you can provide your own _id: 123 (Efficient scaling, do not need to increment from existing ID)
+
+// Mongo Client allows us to connect to a Mongo server
+// const MongoClient = require('mongodb').MongoClient;
+
+// Use destructuring {} to pull of any code from the mongodb library
+// Creates a variable called MongoClient setting it and equal to the MongoClient property of require('mongodb')
+const {MongoClient, ObjectID} = require('mongodb'); // Identical to line 5
+
+// Create a new instance of ObjectID
+// var obj = new ObjectID();
+// console.log(obj);
+
+// ES6 Feature: Object Destructuring - make new variable from an object property
+// Object Destructuring lets you pull out properties from an object-creating variable
+// var user = {name: 'andrew', age: 25};
+// // Make a variable and destructure the user object, pulling off the name property, creating a new name variable and set it to whatever the value it is
+// var {name} = user;
+// // Print out the value of the name property/ variable
+// console.log(name);
+
+// In MongoDB, you do not need to create a database first to be able to start using it
+// Db is automatically created upon kickstarting
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  // Return will stop all executions after it, so the 2nd console.log message will not be printed
+  // Or use If-else()
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // InsertOne lets you insert a new document into our collection
+  // Takes 2 arguments: Object and Callback function
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne({
+    name: "Andrew",
+    age: 25,
+    location: "Ohio"
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+    // console.log(JSON.stringify(result.ops, undefined, 2));
+    // Result.ops is an array of all the documents that got inserted
+    // getTimestamp() method return the timestamp creation in GMT
+    console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  });
+
+
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,40 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // Calling Find is only the first step
+  // Find returns a MongoDB cursor/ pointer to those documents
+  // Cursor has tons of methods, one of it is .toArray
+  // .toArray returns a promise
+  // Setup key-value pairs in find() as query parameter
+  // .find({completed: false}
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('59ed49247c24123faec0d233')     // Use ObjectID to query by ID
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch user', err)
+  });
+
+  // db.close();
+});
